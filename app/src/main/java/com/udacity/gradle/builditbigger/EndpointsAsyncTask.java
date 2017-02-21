33b15134dde138aa51604e218a07055a550ea88b
chenhaow <cwang064@ucr.edu>
2017-02-21 10:52:50 -0800

    backend demo

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -0,0 +1,44 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.AsyncTask;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.joke.endpoint.backend.myApi.MyApi;
+
+import java.io.IOException;
+
+/**
+ * udacity-builditbigger
+ * Created on 30/08/2016 by Espace de travail.
+ */
+public class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
+    private static MyApi myApiService = null;
+    private OnTaskCompleted mListener;
+
+    public EndpointsAsyncTask (OnTaskCompleted listener) {
+        this.mListener = listener;
+    }
+
+    @Override
+    protected String doInBackground(Void... params){
+        if(myApiService == null) {
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("https://builditbigger-140415.appspot.com/_ah/api/");
+
+            myApiService = builder.build();
+        }
+
+        try {
+            return myApiService.getJoke().execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+
+        mListener.onTaskCompleted(result);
+    }
+}
---
+++ app/src/main/java/com/udacity/gradle/builditbigger/OnTaskCompleted.java
@@ -0,0 +1,9 @@
+package com.udacity.gradle.builditbigger;
+
+/**
+ * udacity-builditbigger
+ * Created on 01/09/2016 by Espace de travail.
+ */
+public interface OnTaskCompleted {
+    void onTaskCompleted(String result);
+}
